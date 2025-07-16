180f7392083086bff25f74f8b18ff11834c73ad1
minghf85 <3296986532@qq.com>
2025-07-16 19:36:44 +0800

    fix proxy error

--- scripts/run_config.py
+++ scripts/run_config.py
@@ -1,5 +1,5 @@
 """Runs the experiment as configured by the specified configuration file."""
-
+import os
 if __name__ == '__main__':
     from defame.eval.utils import run_experiment
-    run_experiment(config_file_path="in/defame.yaml")
+    run_experiment(config_file_path="config/experiments/verite.yaml")
